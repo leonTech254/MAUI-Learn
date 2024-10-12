86ca988e3deeafadb4120bb077f104054768aef1
leonTech254 <martinleontech23@gmail.com>
2024-10-12 13:19:50 +0300

    -added a splash screen

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiApp1
+﻿using MauiApp1.Screens;
+
+namespace MauiApp1
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new SplashScreen();
         }
     }
 }
---
+++ Screens/SplashScreen.xaml.cs
@@ -0,0 +1,20 @@
+namespace MauiApp1.Screens;
+
+public partial class SplashScreen : ContentPage
+{
+	public SplashScreen()
+	{
+		InitializeComponent();
+	}
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        // Delay for 3 seconds (3000 milliseconds)
+        await Task.Delay(3000);
+
+        // Navigate to the main page (DisplayCards in your case)
+        Application.Current.MainPage = new NavigationPage(new Dashboard());
+    }
+}
